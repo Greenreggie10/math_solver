a54171b2e3359c533e78455854c95b8e20b153e9
Reginald Green <reggie.green10@yahoo.com>
2022-12-20 20:52:00 -0500

    "variable update"

---
+++ src/main/java/org/example/Points.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class Points {
+    public static void main(String[] args) {
+        // Instructions for this workbook are on Learn the Part (see the Udemy Video: Workbook 2.1 to access the link).
+
+        int points =  0;
+
+        System.out.println("Harry was caught wandering the halls. -50 points for Gryffindor");
+        //update points here
+        points = points - 50;
+        System.out.println(points);
+        System.out.println("Harry was being cheeky in class. -3 points for Gryffindor");
+        //update points here
+        points = points - 3;
+        System.out.println(points);
+        System.out.println("Hermione got full marks on Lockhart's quiz. 30 points for Gryffindor");
+        //update points here
+        points = points + 30;
+        System.out.println(points);
+        System.out.println("Ron won the underground chess game. 100 points for Gryffindor");
+        //update points here
+        points = points + 100;
+        System.out.println(points);
+        System.out.println("Harry defeated Quirrell. 60 points for Gryffindor");
+        //update points here
+        points = points + 60;
+        System.out.println(points);
+
+        // Compare your result to what's on Learn the Part.
+
+    }
+}
